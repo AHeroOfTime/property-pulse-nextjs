f6dba6d2796f3f4a11a15654f28720c3f4bec484
Aaron Rodriguez <aaronerdev@gmail.com>
2025-06-09 10:43:34 -0500

    file based routing

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,7 @@
+// ... acts as a catch all. any URL after the id (pulse.com/propertiers/{id}/...) will still load
+// folder must be name [...id]
+const PropertyPage = () => {
+  return <div>Property Page</div>;
+};
+
+export default PropertyPage;
