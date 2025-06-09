2b9b19093bd2b16ec435e72688ea762a3ae420a9
Aaron Rodriguez <aaronerdev@gmail.com>
2025-06-09 15:53:05 -0500

    client component & nav hooks

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,11 @@
 // ... acts as a catch all. any URL after the id (pulse.com/propertiers/{id}/...) will still load
 // folder must be name [...id]
-const PropertyPage = () => {
-  return <div>Property Page</div>;
+
+const PropertyPage = async ({ params }) => {
+  // asynchronous access of 'params.id'
+  const { id } = await params;
+
+  return <div>Property Page {id}</div>;
 };
 
 export default PropertyPage;
