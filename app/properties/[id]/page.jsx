430112876ae7cce7fd2c4ac0c5396af027dec81a
Aaron Rodriguez <aaronerdev@gmail.com>
2025-06-27 16:09:43 -0500

    single property & header image

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,11 +1,39 @@
 // ... acts as a catch all. any URL after the id (pulse.com/propertiers/{id}/...) will still load
 // folder must be name [...id]
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
 
 const PropertyPage = async ({ params }) => {
   // asynchronous access of 'params.id'
   const { id } = await params;
+  await connectDB();
+  const property = await Property.findById(id).lean();
 
-  return <div>Property Page {id}</div>;
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className='container m-auto py-6 px-6'>
+          <Link
+            href='/properties'
+            className='text-blue-500 hover:text-blue-600 flex items-center'
+          >
+            <FaArrowLeft className='mr-2' /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className='bg-blue-50'>
+        <div className='container m-auto py-10 px-6'>
+          <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+            {/* property info */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default PropertyPage;
